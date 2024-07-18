526907defc1c6df48b725efadda524dbc75a7e2c
duongducphuong <phuongddph40819@fpt.edu.vn>
2024-07-18 16:15:34 +0700

    factory_model_seeder

--- php3/resources/views/create.blade.php
+++ php3/resources/views/create.blade.php
@@ -1,21 +1,22 @@
 @include('header')
-<h1>Trang thêm sản phẩm nhoa</h1>
+<h1>Trang thêm sản phẩm</h1>
 <form action="/handle_create" method="POST">
     <label for="" class="form-lable">Tên:</label>
     <input type="" name="name" class="form-control">
 
-    <label for="" class="form-lable">Danh mục:</label>
-    <select name="category_id" id="">
-        @foreach ($listCategory as $cate)
-            <option value="{{ $cate->id }}">{{ $cate->name_cate }}</option>
-        @endforeach
-    </select>
-    <br>
-    <label for="" class="form-lable">Gía:</label>
-    <input type="" name="price" class="form-control">
+        <label for="" class="form-lable">Danh mục:</label>
+        <select name="category_id" id="">
+            @foreach ($listCategory as $cate)
+                <option value="{{ $cate->id }}">{{ $cate->name_cate }}</option>
+            @endforeach
+        </select>
+        <br>
+        <label for="" class="form-lable">Gía:</label>
+        <input type="" name="price" class="form-control">
 
-    <label for="" class="form-lable">Lượt xem:</label>
-    <input type="" name="view" class="form-control">
-    <button class="btn btn-primary">Thêm</button>
-    @csrf
-</form>
+        <label for="" class="form-lable">Lượt xem:</label>
+        <input type="" name="view" class="form-control">
+        <button class="btn btn-primary">Thêm</button>
+        @csrf
+    </form>
+@endsection
